4c5508ccd3f302bee16d24ee4a960f9bb3d5cb2b
vrushp27 <77703818+vrushp27@users.noreply.github.com>
2023-05-06 17:56:29 -0500

    Final push

--- front-end/src/Components/Shared/trashData.js
+++ front-end/src/Components/Shared/trashData.js
@@ -1,35 +1,12 @@
 const trashData = [
-    {
-      id: 1,
-      name: "HAAV Sample Can 1",
-      lastEmptied: "2023-05-01 01:33am",
-      status: "Empty"
-    },
-    {
-      id: 2,
-      name: "Trash Can Demo #2",
-      lastEmptied: "2023-04-30 10:26pm",
-      status: "Full"
-    },
-    {
-      id: 3,
-      name: "Raspberry pi can demo #3",
-      lastEmptied: "2023-04-17 10:20am",
-      status: "Empty"
-    },
-    {
-      id: 4,
-      name: "Vrushs living room can",
-      lastEmptied: "2023-04-17 10:15am",
-      status: "Almost Full"
-    },
-    {
-        id: 5,
-        name: "Garage Room Can",
-        lastEmptied: "2023-04-17 10:10am",
-        status: "Almost Full"
-    }
-  ];
+  fetch('localhost:5000/api/trash-data')
+  .then(response => response.json())
+  .then(data => {
+    const trashData = data;
+    console.log(trashData);
+  })
+  .catch(error => console.error(error));
+]
   
-  export default trashData;
+ export default trashData;
   
